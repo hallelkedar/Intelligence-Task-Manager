5ec699feaf7940000e6972993488a3d0f5fbfbc4
Hallel <hallelkedar000@gmail.com>
2026-06-18 11:22:46 +0300

    Update and fix agent validation routes

--- routes/agent_routes.py
+++ routes/agent_routes.py
@@ -1,12 +1,14 @@
 from database.agent_db import agent_db
-from fastapi import APIRouter
+from database.base_db import BusinessValidationError
+from fastapi import APIRouter, HTTPException
 from pydantic import BaseModel
 from typing import Literal
+from logs.logger_config import logger
 
 class Agent(BaseModel):
     name: str
     specialty: str
-    agent_rank: Literal['Junior', 'Senior', 'Commander']
+    agent_rank: str
 
 class UpdateAgent(BaseModel):
     name: str | None = None
@@ -17,10 +19,13 @@ class UpdateAgent(BaseModel):
 router = APIRouter()
 
 
-@router.post('')
+@router.post('', status_code=201)
 def create_agent(data: Agent):
-    agent = data.model_dump()
-    return agent_db.create_agent(agent)
+    try:
+        agent = data.model_dump()
+        return agent_db.create_agent(agent)
+    except BusinessValidationError:
+        raise HTTPException(400, 'Agent rank must be - Junior / Senior / Commander')
 
 @router.get('')
 def get_all_agents():
@@ -28,17 +33,32 @@ def get_all_agents():
 
 @router.get('/{id}')
 def get_agent(id: int):
+    if not agent_db.get_agent_by_id(id):
+        raise HTTPException(404, f'Agent not found: {id}')
     return agent_db.get_agent_by_id(id)
 
 @router.put('/{id}')
 def update_agent(id: int, data: UpdateAgent):
-    update = data.model_dump(exclude_unset=True)
-    return agent_db.update_agent(id, update)
+    if not agent_db.get_agent_by_id(id):
+        raise HTTPException(404, f'Agent not found: {id}')
+    
+    data_dict = data.model_dump(exclude_unset=True)
+    if not data_dict:
+        raise HTTPException(422, 'Your body request is empty.')
+    updated = agent_db.update_agent(id, data_dict)
+    if not updated:
+        raise HTTPException(500, 'Internal server error')
+    return {'detail': updated}
 
 @router.put('/{id}/deactivate')
 def deactivate_agent(id: int):
+    if not agent_db.get_agent_by_id(id):
+        raise HTTPException(404, f'Agent not found: {id}')
     return agent_db.deactivate_agent(id)
 
 @router.get('/{id}/performance')
 def get_agent_performance(id: int):
+    if not agent_db.get_agent_by_id(id):
+        raise HTTPException(404, f'Agent not found: {id}')
+    
     return agent_db.get_agent_performance(id)
